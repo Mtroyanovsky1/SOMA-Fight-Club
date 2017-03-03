ec19607ee5a284ad3ed23a8615a1c2a3fdfa4e34
Irvin <irvink.tang@gmail.com>
2017-03-02 23:23:28 -0800

    Starting characters file

---
+++ SOMAFight/characters.js
@@ -0,0 +1,15 @@
+//Characters
+function Character(name, ability, image) {
+  this.charName = name;
+  this.ability = ability;
+  this.charImg = image;
+}
+
+var abilities = {
+
+}
+
+var Moose = new Character('Moose',)
+
+
+module.exports = {}
